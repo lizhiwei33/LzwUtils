7c7fcbd5dfb44e444e79b8e27ce7b1d22d0cd556
lizhiwei33 <lizhiwei_1234@sina.com>
2019-02-27 10:53:31 +0800

    提交控制类

---
+++ app/src/main/java/com/lzw/lzwutils/utils/ToastUtil.java
@@ -0,0 +1,12 @@
+package com.lzw.lzwutils.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    public static void showMsg(Context context,String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+
+    }
+}
